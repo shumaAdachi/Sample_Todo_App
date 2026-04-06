8c5e19b1e4f0a3e081c781e9ad3636136e9112fe
shumaAdachi <adachi.shuma@it-sp.co.jp>
2026-04-06 15:49:16 +0900

    カレンダ側の編集不可

--- Sample_App/Views/EditTodoView.swift
+++ Sample_App/Views/EditTodoView.swift
@@ -5,9 +5,21 @@ struct EditTodoView: View {
 
     var body: some View {
         Form {
-            TextField("タイトルを編集", text: $todo.タイトル)
-            DatePicker("日付を編集", selection: $todo.日付, displayedComponents: .date)
-            Toggle("完了", isOn: $todo.完了)
+            Section(header: Text("基本情報")) {
+                TextField("タイトルを編集", text: $todo.タイトル)
+                DatePicker("日付を編集", selection: $todo.日付, displayedComponents: .date)
+                Toggle("完了", isOn: $todo.完了)
+            }
+
+            Section(header: Text("時間")) {
+                DatePicker("開始時間", selection: $todo.開始時間, displayedComponents: .hourAndMinute)
+                DatePicker("終了時間", selection: $todo.終了時間, displayedComponents: .hourAndMinute)
+            }
+        }
+        .onChange(of: todo.開始時間) { newValue in  // ←ここ！！
+            if todo.終了時間 < newValue {
+                todo.終了時間 = newValue
+            }
         }
         .navigationTitle("編集")
     }
--- Sample_App/Views/TodoDetailView.swift
+++ Sample_App/Views/TodoDetailView.swift
@@ -7,18 +7,25 @@ struct TodoDetailView: View {
     var body: some View {
         Form {
             Section(header: Text("基本情報")) {
-                TextField("タイトル", text: $todo.タイトル)
-                DatePicker("日付", selection: $todo.日付, displayedComponents: .date)
+                TextField("タイトル", text: .constant(todo.タイトル))
+                    .disabled(true)
+
+                DatePicker("日付", selection: .constant(todo.日付), displayedComponents: .date)
+                    .disabled(true)
             }
 
             Section(header: Text("時間設定")) {
-                DatePicker("開始", selection: $todo.開始時間, displayedComponents: .hourAndMinute)
-                DatePicker("終了", selection: $todo.終了時間, displayedComponents: .hourAndMinute)
+                DatePicker("開始", selection: .constant(todo.開始時間), displayedComponents: .hourAndMinute)
+                    .disabled(true)
+
+                DatePicker("終了", selection: .constant(todo.終了時間), displayedComponents: .hourAndMinute)
+                    .disabled(true)
             }
 
             Section(header: Text("メモ")) {
-                TextEditor(text: $todo.備考)
+                TextEditor(text: .constant(todo.備考))
                     .frame(minHeight: 100)
+                    .disabled(true)
             }
         }
         .navigationTitle("予定の詳細")
